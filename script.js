bb8bed0fc4f1b7d9c307011105ba69bc969afd7d
berksarimese <berksarimese@hotmail.com>
2023-01-19 18:27:38 +0300

    Mechanic Improve

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
+//ROTATE AND EQUAL CHECK
+let clickList = [];
+
 $(document).ready(function(){
     $(document).on('click', '.card', function () {
-    $(this).children('.card-front').toggleClass('card-front-rotate');
-    $(this).children('.card-back').toggleClass('card-back-rotate');
+        if(!$(this).children('.card-front').hasClass('card-front-rotate')) {
+        $(this).children('.card-front').toggleClass('card-front-rotate');
+        $(this).children('.card-back').toggleClass('card-back-rotate');
+        
+        if(clickList.length == 2) {
+
+            let object1 = $(clickList[0]).children('.card-back').css('background-image');
+            let object2 = $(clickList[1]).children('.card-back').css('background-image');
+           
+            if(object1 == object2) {
+                clickList = [];
+            }
+
+            for(var j = 0; j < clickList.length; j++) {
+                $(clickList[j]).children('.card-front').toggleClass('card-front-rotate');
+                $(clickList[j]).children('.card-back').toggleClass('card-back-rotate');
+            }
+            clickList = [];
+        }
+        clickList.push($(this));
+        console.log(clickList);
+    }
     });
 
 });
 
-
-let cardList = "";
-
+//AJAX
+let cardDiv = "";
+let cardList = [];
 $.ajax({
     url: "cardData.json",
     data: "{}",
     dataType: "json",
     success: function (data) {
-        console.log(Object.keys(data.users).length);
-        $.each(data.users, function(){
-            const asd = "/";
-            cardList += ` 
-            <div class="card">
-                <div class="card-front"></div>
-                <div class="card-back" style="background-image:url(images/${this.image})"></div>
-            </div> 
-            `;
-            /*
-            let cardDiv = $(".main-container").append("<div></div>");
-            $(cardDiv).addClass("card");*/
+        console.log(Object.keys(data.cards).length);
+        for(var k = 0; k < 2; k++) {
+            $.each(data.cards, function(){
 
-            console.log(this.name, this.surname, this.image);
-        });
-        $(".main-container").append(cardList);
+                cardList.push(this);
+            
+            });
+        }
+        shuffle(cardList);
+        listCards();
+
     },
     error: function (result) {
         console.log("hata");
     }
 });
 
+//CARD LİSTİNG
+function listCards() {
+    $('.main-container').html("");
+    for(var i = 0; i < cardList.length; i++) {
 
-
-
-
-
-
-
-
-
-//Something else 
-/*
-    let divide1 = 0;
-    let divide2 = 0;
-    let max = 30;
-
-for (let i = 1;i<=max;i++) {
-    divide1 = i / 3;
-    divide2 = i / 5;
-
-    if(Number.isInteger(divide1) && Number.isInteger(divide2)) {
-        
-        console.log('FizzBuzz');
-
-    } else {
-
-        if(Number.isInteger(divide1)) {
-            console.log('Fizz');
-            
-        } else {
-
-            if(Number.isInteger(divide2)) {
-                console.log('Buzz');
-
-            } else {
-
-                console.log(i);
-
-            }
-        }
+        cardDiv += ` 
+            <div class="card">
+                <div class="card-front"></div>
+                <div class="card-back" style="background-image:url(images/${cardList[i].image})"></div>
+            </div> 
+            `;
     }
+
+    $(".main-container").append(cardDiv);
 }
-*/
+
+//ARRAY SHUFFLER
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
